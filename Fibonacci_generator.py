4b9da97afe8d4b9a439d989a9d35180090a1141c
Michael McClain <michaelmcclain@Michaels-MBP.attlocal.net>
2018-09-03 21:07:46 -0700

    First commit

---
+++ Fibonacci_generator.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+# I want put in a number that gives how many fibonacci numbers I get out
+
+print ("How many fibonancci numbers?")
+n = input('Enter the number here: ')
+
+
+# I got to have the fibonacci equasion
+
+# Function for nth Fibonacci number
+ 
+def Fibonacci(n):
+    if n<0:
+        print("Incorrect input")
+	return ""
+    # First Fibonacci number is 0
+    elif n==1:
+        return 0
+    # Second Fibonacci number is 1
+    elif n==2:
+        return 1
+    else:
+        return Fibonacci(n-1)+Fibonacci(n-2)
+# Driver Program
+
+
+
+
+
+i = 1
+fiblist = []
+if n <= 0:
+	print("Incorrect Input")
+else:
+
+	while i <= n:
+		fiblist.append(Fibonacci(i+1))
+		i = i + 1
+	print (fiblist)
+	
+	
+ 
+	
+
+
---
+++ file02.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+print("Hello")
+
+file = open("frog.jpg","r")
+cont = file.read()
+print(cont)
